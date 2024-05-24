c29dfb9eaf9b669b2acadf1693a0f8abbe3afedc
Harshana_P_Semasinghe <harshanaprabhathsemasinghe@gmail.com>
2024-05-24 20:57:31 +0530

    "Sign-in and Sign-up done"

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 import { Sedan } from "next/font/google";
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 const sedan = Sedan({ weight: "400", subsets: ["latin"] });
@@ -7,28 +9,56 @@ const sedan = Sedan({ weight: "400", subsets: ["latin"] });
 
 
 export default function Page() {
+
+  
+
+
+
   return (
 
    
 
-      <div className="flex flex-col items-center justify-center p-1 gap-1 custom-gradient absolute top-0 left-0 h-full w-full rounded-lg">
+    <div className="flex  items-center justify-center p-1 gap-1  absolute top-0 left-0 h-full w-full rounded-lg sign-in-cover">
 
          
-        <div className="flex flex-col gap-3 items-center justify-center">
+        
+   
 
-          <h1 className={`text-6xl italic text-slate-200 ${sedan.className}`} >TODOAPP</h1>
-          <h3 className="text-xl">Good to see you again !</h3>
+          <SignIn path="/sign-in"    />
 
-        </div>
-       
+      <div className="flex flex-col gap-1  bg-red-600  sign-in-div hidden">
+        <div className="opacity-25 fixed top-[100px]  right-0  -z-20">
 
-        <SignIn path="/sign-in"  />
+            <Image
+
+            src='/signin.svg'
+            width={350}
+            height={350}
+            alt='signup'
+            />
+          </div>
+
+          <div className="mx-auto z-20 flex flex-col gap-5 items-center mt-32">
+
+            <h1 className="text-4xl text-white font-bold text-center">Welcom Back!</h1>
+            <p className="w-[240px] text-center">Please sign-in with your personal info to embrk the journey!</p>
+            <Link href="/sign-up"><button className="bg-white text-black p-2 rounded-lg w-[150px]">Sign up</button></Link>
+            
+            
+
+          </div>
+        
+
+        
+        
 
       </div>
 
-      
+      </div>
 
-    
+            
+
+          
     
 
   
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 
@@ -9,18 +11,41 @@ export default function Page() {
 
     
     
-    <div className="flex flex-col items-center justify-center p-1 gap-1 bg-red-200 absolute top-0 left-0 h-full w-full rounded-lg">
+    <div className="flex  items-center justify-center p-1 gap-1  absolute top-0 left-0 h-full w-full rounded-lg sign-up-cover">
 
          
-        <div className="flex flex-col gap-1 items-center justify-center">
+        
+   
+
+            <SignUp path="/sign-up"  />
+
+        <div className="flex flex-col gap-1  bg-red-600  sign-up-div hidden">
+          <div className="opacity-50 fixed top-[350px]  -right-0   -z-20">
+
+              <Image
+
+              src='/signup.svg'
+              width={350}
+              height={350}
+              alt='signup'
+              />
+            </div>
+
+            <div className="mx-auto z-20 flex flex-col gap-5 items-center mt-40">
+
+              <h1 className="text-4xl text-white font-bold text-center">Hello, Friend!</h1>
+              <p className="w-[240px] text-center">Enter you Credentials and start the journey !</p>
+              <Link href="/sign-in"><button className="bg-white text-black p-2 rounded-lg w-[150px]">Sign In</button></Link>
+              
+              
+
+            </div>
+          
 
           
           
 
         </div>
-   
-
-            <SignUp path="/sign-up"  />
 
     </div>
   
